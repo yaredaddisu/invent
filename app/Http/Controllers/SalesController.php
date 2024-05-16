174eb3dfa64f3e21ca9811f0895fe41c47ffd649
yared <34055381+yaredaddisu@users.noreply.github.com>
2024-05-16 11:11:33 -0700

    update

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -15,31 +15,52 @@ class SalesController extends Controller
         $dateFilter = $request->keyword;
 
         $query = Survey::with(['category', 'stockOut' => function ($query) use ($dateFilter) {
-            switch($dateFilter) {
+            switch($dateFilter){
                 case 'today':
-                    $query->whereDate('created_at', Carbon::today());
+                    $query->whereDate('created_at',Carbon::today());
                     break;
                 case 'yesterday':
-                    $query->whereDate('created_at', Carbon::yesterday());
+                    $query->whereDate('created_at',Carbon::yesterday());
                     break;
                 case 'this_week':
-                    $query->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]);
+                    $query->whereBetween('created_at',[Carbon::now()->startOfWeek(),Carbon::now()->endOfWeek()]);
                     break;
-                case 'last_week':
-                    $query->whereBetween('created_at', [Carbon::now()->subWeek()->startOfWeek(), Carbon::now()->subWeek()->endOfWeek()]);
-                    break;
-                case 'this_month':
-                    $query->whereMonth('created_at', Carbon::now()->month);
-                    break;
-                case 'last_month':
-                    $query->whereMonth('created_at', Carbon::now()->subMonth()->month);
-                    break;
-                case 'this_year':
-                    $query->whereYear('created_at', Carbon::now()->year);
-                    break;
-                case 'last_year':
-                    $query->whereYear('created_at', Carbon::now()->subYear()->year);
-                    break;
+                    case 'january':
+                        $query->whereMonth('created_at', 1);
+                        break;
+                    case 'february':
+                        $query->whereMonth('created_at', 2);
+                        break;
+                    case 'march':
+                        $query->whereMonth('created_at', 3);
+                        break;
+                    case 'april':
+                        $query->whereMonth('created_at', 4);
+                        break;
+                    case 'may':
+                        $query->whereMonth('created_at', 5);
+                        break;
+                    case 'june':
+                        $query->whereMonth('created_at', 6);
+                        break;
+                    case 'july':
+                        $query->whereMonth('created_at', 7);
+                        break;
+                    case 'august':
+                        $query->whereMonth('created_at', 8);
+                        break;
+                    case 'september':
+                        $query->whereMonth('created_at', 9);
+                        break;
+                    case 'october':
+                        $query->whereMonth('created_at', 10);
+                        break;
+                    case 'november':
+                        $query->whereMonth('created_at', 11);
+                        break;
+                    case 'december':
+                        $query->whereMonth('created_at', 12);
+                        break;
             }
             // Filter only stock out transactions
             $query->where('Transaction', 'Stock Out');
